a73563696106b285b0d9200bfb02364f3e31403e
10zinten <ten13zin@gmail.com>
2023-04-07 17:35:06 +0530

    fix bugs

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ def align():
 def run():
     demo = gr.Interface(
         fn=align,
-        inputs=gr.inputs.JSON(
+        inputs=gr.JSON(
             value={
                 "bo_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-bo.txt",
                 "en_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-en.txt"
             }
         ),
-        outputs=gr.outputs.JSON(),
+        outputs=gr.JSON(),
     )
 
     demo.launch(server_name="0.0.0.0", server_port=7860)
