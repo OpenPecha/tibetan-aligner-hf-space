d6ace58f3e67f5914521608c01719a519c98b924
10zinten <ten13zin@gmail.com>
2023-04-07 17:15:50 +0530

    use gradio

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+import gradio as gr
+import requests
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+
+def align():
+    return {"success": True}
+
+
+def run():
+    demo = gr.Interface(
+        fn=align,
+        inputs=gr.inputs.JSON(
+            value={
+                "bo_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-bo.txt",
+                "en_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-en.txt"
+            }
+        )
+        outputs=gr.outputs.JSON(),
+    )
+
+    demo.launch(server_name="0.0.0.0", server_port=7860)
+
+
+if __name__ == "__main__":
+    run()
