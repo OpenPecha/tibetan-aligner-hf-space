9db4af5509990d0061fa1703215e7609aab0f5c3
10zinten <ten13zin@gmail.com>
2023-04-11 16:05:56 +0530

    import tibetan-aligner source

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 import os
 import re
+import shutil
+import stat
 import subprocess
 import tempfile
+import uuid
+from contextlib import contextmanager
 from pathlib import Path
 
 import gradio as gr
 import requests
 
-DEV = os.environ.get("DEV", False)
+ALIGNER_SCRIPT_DIR = Path("./tibetan-aligner").resolve()
+ALIGNER_SCRIPT_NAME = "align_tib_en.sh"
+ALIGNER_SCRIPT_PATH = ALIGNER_SCRIPT_DIR / ALIGNER_SCRIPT_NAME
+assert ALIGNER_SCRIPT_PATH.is_file()
+
+
+def make_dir_executable(dir_path: Path):
+    for fn in dir_path.iterdir():
+        st = os.stat(fn)
+        os.chmod(fn, st.st_mode | stat.S_IEXEC)
+        st = os.stat(fn)
+        os.chmod(fn, st.st_mode | stat.S_IXGRP)
+        st = os.stat(fn)
+        os.chmod(fn, st.st_mode | stat.S_IXOTH)
+
+
+make_dir_executable(ALIGNER_SCRIPT_DIR)
+
+
+@contextmanager
+def TemporaryDirectory():
+    tmpdir = Path("./output").resolve() / uuid.uuid4().hex[:8]
+    tmpdir.mkdir(exist_ok=True, parents=True)
+    try:
+        yield tmpdir
+    finally:
+        shutil.rmtree(str(tmpdir))
 
 
 def download_file(url, output_path=Path("./data")):
@@ -22,10 +52,10 @@ def download_file(url, output_path=Path("./data")):
 
 
 def _run_align_script(bo_fn, en_fn, output_dir):
-    aligner_script_dir = "../tibetan-aligner"
-    cmd = ["../tibetan-aligner/align_tib_en.sh", bo_fn, en_fn, output_dir]
+    cmd = [str(ALIGNER_SCRIPT_PATH), str(bo_fn), str(en_fn), str(output_dir)]
+    print(cmd)
     output = subprocess.run(
-        cmd, check=True, capture_output=True, text=True, cwd=aligner_script_dir
+        cmd, check=True, capture_output=True, text=True, cwd=str(ALIGNER_SCRIPT_DIR)
     )
     output_fn = re.search(r"\[OUTPUT\] (.*)", output.stdout).group(1)
     output_fn = "/" + output_fn.split("//")[-1]
@@ -33,7 +63,7 @@ def _run_align_script(bo_fn, en_fn, output_dir):
 
 
 def align(file_urls):
-    with tempfile.TemporaryDirectory() as tmpdir:
+    with TemporaryDirectory() as tmpdir:
         output_dir = Path(tmpdir)
         bo_fn = download_file(file_urls["bo_file_url"], output_dir)
         en_fn = download_file(file_urls["en_file_url"], output_dir)
@@ -41,32 +71,20 @@ def align(file_urls):
         return Path(aligned_fn).read_text()
 
 
-if DEV:
-    with gr.Blocks() as demo:
-        input = gr.JSON(
-            value={
-                "bo_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-bo.txt",
-                "en_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-en.txt",
-            }
-        )
-        output = gr.TextArea()
-        align_btn = gr.Button("Align")
-        align_btn.click(
-            fn=align,
-            inputs=input,
-            outputs=output,
-            api_name="align",
-        )
-else:
-    demo = gr.Interface(
+with gr.Blocks() as demo:
+    input = gr.JSON(
+        value={
+            "bo_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-bo.txt",
+            "en_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-en.txt",
+        }
+    )
+    output = gr.TextArea()
+    align_btn = gr.Button("Align")
+    align_btn.click(
         fn=align,
-        inputs=gr.JSON(
-            value={
-                "bo_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-bo.txt",
-                "en_file_url": "https://raw.githubusercontent.com/OpenPecha/tibetan-aligner/main/tests/data/text-en.txt",
-            }
-        ),
-        outputs=gr.TextArea(),
+        inputs=input,
+        outputs=output,
+        api_name="align",
     )
 
 
---
+++ import_tibetan_aligner_source.py
@@ -0,0 +1,25 @@
+import shutil
+import sys
+from pathlib import Path
+
+NON_SOURCES_FILES = [
+    ".",
+    "..",
+    ".git",
+    ".github",
+    ".gitignore",
+    ".venv",
+    ".idea",
+    "Dockerfile",
+    "__pycache__",
+]
+
+if __name__ == "__main__":
+    source_dir = Path(sys.argv[1])
+    dest_dir = Path(__file__).parent / source_dir.name
+    dest_dir.mkdir(exist_ok=True)
+    for fn in source_dir.iterdir():
+        if fn.name in NON_SOURCES_FILES:
+            continue
+        dest_fn = dest_dir / fn.name
+        shutil.copy2(str(fn), str(dest_fn))
